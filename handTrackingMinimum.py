dcaa7fc3f446950c0cf4332e423d90db2067dc5c
shayad_mayank <mayank.mchandani3@gmail.com>
2026-02-28 19:59:21 +0530

    Full Hand tracking and marking

--- handTrackingMinimum.py
+++ handTrackingMinimum.py
@@ -10,8 +10,11 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Hand skeleton connections
 HAND_CONNECTIONS = [
-      # Index
-    (9,10),(10,11),(11,12)                     # Palm base
+    (0,9),(9,10),(10,11),(11,12), #middle
+    (0,5),(5,6),(6,7),(7,8), #index
+    (0,1),(1,2),(2,3),(3,4), #thumb
+    (0,13),(13,14),(14,15),(15,16),
+    (0,17),(17,18),(18,19),(19,20)
 ]
 
 # Landmarker options (VIDEO mode - synchronous)
